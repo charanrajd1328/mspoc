5a26c0e2a7ebc30ddf00a96eb3d6f3ae143717a0
w2cluster454 <w2cluster454@w2cluster503.iiht.tech>
2020-11-21 19:15:17 -0500

    Adding Eureka server,CurrencyMS and ExcCurrencyMS

---
+++ eurekaserver/src/test/java/com/ibm/msactivity/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.msactivity.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
